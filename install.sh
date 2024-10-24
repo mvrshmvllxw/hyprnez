f779fa26693e592e0142e96050b453ac789a0181
mvrshmvllxw <mvrshmvllxw@github.com>
2024-10-25 00:51:11 +0300

    pre-release v0.1

--- install.sh
+++ install.sh
@@ -65,6 +65,7 @@ cp -rf .config/* ~/.config/
 cp -rf .themes/* ~/.themes/
 sudo cp -rf usr/* /usr/
 cp -rf wallpapers/* ~/Pictures/Wallpapers/
+cp -rf live/* ~/Pictures/Live
 echo "::: Done."
 
 ###################
@@ -199,7 +200,7 @@ paru -S --needed --noconfirm papirus-icon-theme tela-circle-icon-theme-all bibat
 # Apps and utils
 paru -S --needed --noconfirm mako network-manager-applet bluez bluez-utils blueman brightnessctl udiskie xdg-desktop-portal-hyprland \
 xdg-desktop-portal-gtk nautilus dolphin gnome-text-editor waybar swww nwg-shell tofi wlogout wayshot archlinux-xdg-menu \
-mpvpaper fish kitty lsd starship krabby less hypridle hyprlock jq 
+mpvpaper fish kitty lsd starship krabby less hypridle hyprlock jq xdg-desktop-portal-kde
 # View apps
 paru -S --needed --noconfirm mpv imv code code-features code-marketplace firefox telegram-desktop
 # System utils
